2e5503c61aeb95f3803e60985f286ecbae6342c0
Viet Anh <vietanhle3012bn@gmail.com>
2025-12-19 16:20:37 +0700

    feat: gen data by day, s2 merge new data

--- airflow/dags/spark/s2_merge.py
+++ airflow/dags/spark/s2_merge.py
@@ -13,22 +13,39 @@ class S2_Merge(PythonSparkJob):
         super().__init__(ps, spark_conf)
         self.bronze_path = ps.get_param("bronze", required=True)
         self.gold_fdata_path = ps.get_param("gold-fdata", required=True)
+        base_bucket = self.gold_fdata_path.split("/delta/")[0]
+        self.checkpoint_path = f"{base_bucket}/checkpoints/s2_merge"
     def run(self):
         spark_session = self.create_spark_session(app_name=APP_NAME)
-        self._run_inner(spark_session, self.bronze_path, self.gold_fdata_path)
-    def _run_inner(self, spark_session: SparkSession, bronze_path: str, gold_fdata_path: str):
-        bronze_df = spark_session.read.format("delta").load(bronze_path)
-    
-        if not DeltaTable.isDeltaTable(spark_session, gold_fdata_path):
-            bronze_df.write.format('delta').mode('overwrite').partitionBy("year", 'month').save(gold_fdata_path)
+        self._run_inner(spark_session, self.bronze_path, self.gold_fdata_path, self.checkpoint_path)
+    def _run_inner(self, spark_session: SparkSession, bronze_path: str, gold_fdata_path: str, checkpoint_path: str):
+        def upsert_to_gold(micro_batch_df, batch_id):
+            if micro_batch_df.count() == 0:
+                return
+            
+            deduplicated_batch = micro_batch_df.dropDuplicates(["stopId", "datetime"])
+
+            if not DeltaTable.isDeltaTable(spark_session, gold_fdata_path):
+                deduplicated_batch.write.format('delta').mode('overwrite').partitionBy("year", 'month').save(gold_fdata_path)
+                return
+
+            fdata_table = DeltaTable.forPath(spark_session, gold_fdata_path)
+            
+            fdata_table.alias('t').merge(
+                deduplicated_batch.alias('s'), 
+                "t.stopId = s.stopId AND t.datetime = s.datetime"
+            ).whenMatchedUpdateAll() \
+             .whenNotMatchedInsertAll() \
+             .execute()
+
+        bronze_stream = spark_session.readStream.format("delta").load(bronze_path)
         
-        fdata_table = DeltaTable.forPath(spark_session, gold_fdata_path)
+        query = bronze_stream.writeStream.format('delta').foreachBatch(upsert_to_gold) \
+            .option("checkpointLocation", checkpoint_path) \
+            .trigger(availableNow=True) \
+            .start()
         
-        fdata_table.alias('t').merge(bronze_df.alias('s'), "t.stopId =  s.stopId AND t.datetime = s.datetime") \
-            .whenMatchedUpdateAll() \
-                .whenNotMatchedInsertAll() \
-                    .execute()
-                    
+        query.awaitTermination()        
         spark_session.stop()
 
 def main():
